d2ffd6e142c0fdcacc4df47acd87a900a6b100a7
Bakautov <78703011+Bakautov@users.noreply.github.com>
2021-06-10 19:58:46 +0700

    Add a schematic display of the web page

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo App</h1>
+      <div>
+        <label>Title:</label>
+        <input placeholder = " Enter the Title: "></input>
+      </div>
+      <button>Add</button>
     </div>
   );
 }
-
 export default App;
