370f9142c5faf593585df99b72776671243d4d9e
Evgeny Irikov <evgen5060@gmail.com>
2021-06-13 18:47:46 +0700

    Add style to trash icon

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -12,7 +12,9 @@ const Todo = ({ todo, deleteTodo }: IProps) => {
   return (
     <div className="flex w-full items-center p-5">
       <span className="">{todo.title}</span>
+	<div className="flex-grow"></div>
 	<FontAwesomeIcon
+	className="cursor-pointer fill-current text-red-300 hover:text-red-400"
         onClick={() => {
           deleteTodo(todo.title);
         }}
